d5a4ae402882585bd73a03694273fe4fcd84fc68
Itamar Hata <itamar.viana@gmail.com>
2019-06-06 22:33:43 -0300

    Fixing old js

---
+++ js/sound-manager.js
@@ -0,0 +1,76 @@
+/**
+ * Conects to to microphone, and starts to listening it.
+ */
+class SoundManager {
+    /**
+     * 
+     * @param {StrokeDetection} stroke_detection 
+     */
+    constructor(stroke_detection) {
+        this.stroke_detection = stroke_detection;
+        this.analyser = null;
+        this.stream = null;
+        this.audio_context = null;
+        this.started = false;
+    }
+
+    StartListeningMicrophone() {
+        if (this.started) {
+            this.Stop();
+            return;
+        }
+        this.started = true;
+        var constraints = { audio: true };
+        var obj = this;
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(function (stream) {
+                var javascriptNode = obj.GetJavascriptNode(stream);
+                javascriptNode.onaudioprocess = function () {
+                    var array_size = obj.analyser.fftSize;
+                    var sound_data = new Uint8Array(array_size);
+                    obj.analyser.getByteTimeDomainData(sound_data);
+                    obj.stroke_detection.OnAudioProcess(sound_data);
+                }
+            })
+            .catch(function (err) { console.log(err.name + ": " + err.message); });
+    }
+
+    Stop() {
+        this.stroke_detection.ClearCanvas();
+        this.started = false;
+        if (this.audio_context.state === 'closed') {
+            return;
+        }
+        try {
+            this.stream.getTracks()[0].stop();
+            this.audio_context.close(); // returns a promise;
+        } catch (ex) {
+            // This fails in some older versions of chrome. Nothing we can do about it.
+        }
+    }
+
+    GetJavascriptNode(stream) {
+        this.stream = stream;
+        this.audio_context = new AudioContext();
+
+        this.analyser = this.audio_context.createAnalyser();
+        var microphone = this.audio_context.createMediaStreamSource(stream);
+        var javascript_node = this.audio_context.createScriptProcessor(4096, 1, 1);
+
+        this.analyser.smoothingTimeConstant = 0.8;
+        this.analyser.fftSize = 2048;
+
+        // Create a Low Pass Filter to Isolate Low End Beat
+        var filter = this.audio_context.createBiquadFilter();
+        filter.type = "lowpass";
+        filter.frequency.value = 140;
+        microphone.connect(filter);
+        filter.connect(this.analyser);
+
+        //microphone.connect(analyser);
+        this.analyser.connect(javascript_node);
+        javascript_node.connect(this.audio_context.destination);
+
+        return javascript_node;
+    }
+}
---
+++ js/stroke-detection.js
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+
+class StrokeDetection {
+    /**
+     * 
+     * @param {Canvas} canvas_context to draw the sound waves.
+     */
+    constructor(canvas_context, canvas_color) {
+        this.canvas_context = canvas_context;
+        this.canvas_color = canvas_color;
+        this.last_stroke = Date.now();
+        this.stroke_count = 0;
+    }
+
+    OnAudioProcess(sound_data) {
+        let time_diff = Date.now() - this.last_stroke;
+        if (time_diff < 80) {
+            return;
+        }
+
+        this.DrawSoundWave(sound_data);
+        let max_value = Math.max.apply(null, sound_data) / 128.0;
+        if (max_value > 1.2) {
+            this.stroke_count += 1;
+            $("#stroke-count").text("" + this.stroke_count);
+            $('#table-debug').append(`<tr><td>${time_diff} ${max_value}</td></tr>`);
+        }
+
+
+        this.last_stroke = Date.now();
+        return;
+    }
+
+    ClearCanvas() {
+        this.stroke_count = 0;
+        $("#stroke-count").text("" + this.stroke_count);
+        let width = this.canvas_context.canvas.width;
+        let height = this.canvas_context.canvas.height;
+        this.canvas_context.clearRect(0, 0, width, height);
+    }
+
+    DrawSoundWave(sound_data) {
+        if (sound_data.length == 0) {
+            return;
+        }
+        this.canvas_context.fillStyle = this.canvas_color;
+        let width = this.canvas_context.canvas.width;
+        let height = this.canvas_context.canvas.height;
+        this.canvas_context.fillRect(0, 0, width, height);
+
+        this.canvas_context.lineWidth = 2;
+        this.canvas_context.strokeStyle = "rgb(0, 0, 0)";
+
+        this.canvas_context.beginPath();
+
+        let slice_width = width * 1.0 / sound_data.length;
+        let x = 0;
+
+        for (let i = 0; i < sound_data.length; i++) {
+
+            let v = sound_data[i] / 128.0;
+            let y = v * height / 2;
+
+            if (i === 0) {
+                this.canvas_context.moveTo(x, y);
+            } else {
+                this.canvas_context.lineTo(x, y);
+            }
+
+            x += slice_width;
+        }
+
+        this.canvas_context.lineTo(width, height / 2);
+        this.canvas_context.stroke();
+    }
+}
+
+// Export node module.
+if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+    module.exports = StrokeDetection;
+}
